e59efc0b2357fff97696913e30dc38d138329998
Jonathan García <jona03g97@gmail.com>
2022-07-25 21:36:57 -0600

    Add: Categories selection

--- frontend/src/components/Categories/index.js
+++ frontend/src/components/Categories/index.js
@@ -1,24 +1,25 @@
 import { FaThLarge, FaTshirt } from 'react-icons/fa'
 import { GiRunningShoe } from 'react-icons/gi'
 import { BsSmartwatch } from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import changeCategory from '../../utils/changeCategory'
 import { Categories, Item } from './styles'
 
 const CategoriesComponent = () => {
   const navigate = useNavigate()
+  const params = useParams()
 
   return (
     <Categories className='categories'>
       <Item
-        className='selected'
+        className={`categories__item ${!params.id && 'selected'}`}
         onClick={(event) => changeCategory({ event, navigate })}
       >
         <FaThLarge />
         <p>Todo</p>
       </Item>
       <Item
-        className='categories__item'
+        className={`categories__item ${params.id === '0' && 'selected'}`}
         id={0}
         onClick={(event) => changeCategory({ event, navigate })}
       >
@@ -26,7 +27,7 @@ const CategoriesComponent = () => {
         <p>Calzado</p>
       </Item>
       <Item
-        className='categories__item'
+        className={`categories__item ${params.id === '1' && 'selected'}`}
         id={1}
         onClick={(event) => changeCategory({ event, navigate })}
       >
@@ -34,7 +35,7 @@ const CategoriesComponent = () => {
         <p>Relojes</p>
       </Item>
       <Item
-        className='categories__item'
+        className={`categories__item ${params.id === '2' && 'selected'}`}
         id={2}
         onClick={(event) => changeCategory({ event, navigate })}
       >
