27e25966fad02cda41557c4b2a0844d574e5216f
Jonathan García <jona03g97@gmail.com>
2022-08-02 17:35:35 -0600

    Fix: Styling with more products

--- frontend/src/components/Product/index.js
+++ frontend/src/components/Product/index.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { Product as ProductContainer, ColorButton } from './styles'
+import { Link } from 'react-router-dom'
 
-const Product = ({ images, description, name, price, colors, variations }) => {
+const Product = ({ description, name, price, colors, variations }) => {
   const [colorSelected, setColorSelected] = useState(colors[0])
 
   const handleSelectColor = (event) => {
@@ -10,17 +11,19 @@ const Product = ({ images, description, name, price, colors, variations }) => {
 
   return (
     <ProductContainer>
-      <img
-        src={
-          variations.find((variation) => variation.color === colorSelected)
-            .images.presentation
-        }
-        alt={description.small}
-      />
-      <h5>{name}</h5>
-      <p>{description.small}</p>
-      <h6>₡{price}</h6>
-      <div className='new'>Nuevo</div>
+      <Link to={`/product/${variations[0].sku}`}>
+        <img
+          src={
+            variations.find((variation) => variation.color === colorSelected)
+              .images.presentation
+          }
+          alt={description.small}
+        />
+        <h5>{name}</h5>
+        <p>{description.small}</p>
+        <h6>₡{price}</h6>
+        <div className='new'>Nuevo</div>
+      </Link>
       <div className='colors'>
         {colors.map((color) => {
           return (
--- frontend/src/components/Product/styles.js
+++ frontend/src/components/Product/styles.js
@@ -47,47 +47,50 @@ const Product = styled.li`
   box-shadow: ${shadow.main};
   position: relative;
 
-  img {
-    width: 100%;
-    height: 132.5px;
-    margin-bottom: 5px;
-    object-fit: contain;
-  }
+  a {
+    color: initial;
+    text-decoration: none;
+    img {
+      width: 100%;
+      height: 132.5px;
+      margin-bottom: 5px;
+      object-fit: contain;
+    }
 
-  h5 {
-    font-size: 1.8rem;
-    font-weight: 600;
-    padding-bottom: 5px;
-  }
+    h5 {
+      font-size: 1.8rem;
+      font-weight: 600;
+      padding-bottom: 5px;
+    }
 
-  p {
-    font-size: 1.2rem;
-    padding-bottom: 5px;
-    color: gray;
-  }
+    p {
+      font-size: 1.2rem;
+      padding-bottom: 5px;
+      color: gray;
+    }
 
-  h6 {
-    font-size: 1.5rem;
-    font-weight: 500;
-  }
+    h6 {
+      font-size: 1.2rem;
+      font-weight: 300;
+    }
 
-  .new {
-    background-color: ${color.main};
-    width: 50px;
-    padding: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 999px;
-    height: 10px;
-    position: absolute;
-    color: white;
-    font-weight: 400;
-    height: 20px;
-    top: 10px;
-    left: 8px;
+    .new {
+      background-color: ${color.main};
+      width: 50px;
+      padding: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 999px;
+      height: 10px;
+      position: absolute;
+      color: white;
+      font-weight: 400;
+      height: 20px;
+      top: 10px;
+      left: 8px;
+    }
   }
-
   .colors {
     display: flex;
     justify-content: space-evenly;
